a92905e6216c327e283eb48624c8c19270f58abf
abraham-siyanbola <abrahamsiyanbola09@gmail.com>
2026-05-31 21:49:32 +0100

    NairaTransfer Manifests added & Secrets Manager integration

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,7 +1,7 @@
 # ====================== RDS ======================
 resource "aws_db_subnet_group" "main" {
-  name       = "${var.app_name}-db-subnet"
-  subnet_ids = aws_subnet.public[*].id
+  name       = "${var.app_name}-db-subnet-group"
+  subnet_ids = aws_subnet.private[*].id
 }
 
 resource "aws_db_instance" "main" {
@@ -12,7 +12,7 @@ resource "aws_db_instance" "main" {
   allocated_storage      = 20
   username               = "postgres"
   db_name                = "nairatransfer"
-  password               = "ChangeMe123!"
+  password               = jsondecode(aws_secretsmanager_secret_version.app.secret_string)["DB_PASSWORD"]
   skip_final_snapshot    = true
   publicly_accessible    = false
   vpc_security_group_ids = [aws_security_group.rds.id]
